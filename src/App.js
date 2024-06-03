d675c04f94115c61738efdd216ed80dc8c4a52a7
JWanyoa <khamalawanyoa@gmail.com>
2024-06-03 10:16:17 +0300

    Added banner, stylings and overlays

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Carousel } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.css";
-import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 
 import myImage1 from './images/image1.jpg'; // Images for the carousel
 import myImage2 from './images/image2.jpg';
@@ -10,21 +9,36 @@ import myImage3 from './images/image3.jpg';
 import myImage4 from './images/image4.jpg';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   const images = [myImage1, myImage2, myImage3, myImage4];
   const captions = [
-    'Caption for Image 1',
-    'Caption for Image 2',
-    'Caption for Image 3',
-    'Caption for Image 4',
+    'Bring Life to a Dead World',
+    'Captivate the soul with the food of Life',
+    'Increase the Kingdom through creative ministry',
+    'Enhance the culture of Loving God',
+  ];
+
+  const captionParagraphs = [
+    'We stand as vessels of the Ministry of Christ, entrusted with the sacred task of bringing life to a dead world through the Word of God. With unwavering faith and steadfast conviction, we proclaim His truth to dispel darkness and breathe hope into the depths of despair. Through the timeless wisdom of Scripture and the power of His divine message, we illuminate paths of redemption and renewal, infusing barren landscapes with the vibrant promise of salvation.',
+    'We beckon on a journey beyond the mundane, inviting you to indulge in the nourishment of not just the body, but the spirit itself. Like a masterful chef crafting a symphony of flavors, we tantalize your senses and stir your deepest emotions. It\'s more than mere sustenance; it\'s an exquisite blend of nourishment for your body, mind, and soul. Each bite, each experience, is a revelation, unlocking the essence of existence and igniting a passion for living fully.',
+    'We, as faithful stewards of the Ministry of Christ, are called to magnify His Kingdom through the transformative lens of creative ministry. With devotion as our compass and ingenuity as our tool, we illuminate the path of discipleship with innovative approaches and fervent dedication. Our mission is to amplify His teachings, weaving them into the fabric of daily life, and nurturing souls towards spiritual abundance.',
+    'We, as custodians of the Ministry of Christ, are steadfast in our commitment to enhancing the culture of loving God. With reverence as our foundation and compassion as our guide, we cultivate a community where adoration and devotion flourish. Through heartfelt worship, fervent prayer, and acts of unwavering kindness, we deepen our connection to the Divine and nurture a profound love for God within ourselves and others.'
   ];
 
   return (
     <div className="App">
       <header>
-        <h1>
-          <a href="#">Church Name</a>
+        <h1 className=''>
+          <a href="#" className='small'>&nbsp; Antioch Grace Ministries</a>
         </h1>
-        <nav>
+        <div className="hamburger" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+        <nav className={`nav-menu ${menuOpen ? 'open' : ''}`}>
           <a href="#">Home</a>
           <a href="#">About Us</a>
           <a href="#">Ministries</a>
@@ -33,7 +47,7 @@ function App() {
         </nav>
       </header>
 
-      <section className="container">
+      <section className="">
         <Carousel>
           {images.map((image, index) => (
             <Carousel.Item key={index}>
@@ -42,8 +56,9 @@ function App() {
                 src={image}
                 alt={`Image ${index + 1}`}
               />
-              <Carousel.Caption>
-                <h3>{captions[index]}</h3>
+              <Carousel.Caption className='caption-overlay'>
+                <h3 className="caption-title">{captions[index]}</h3>
+                <p className="caption-text">{captionParagraphs[index]}</p>
               </Carousel.Caption>
             </Carousel.Item>
           ))}
